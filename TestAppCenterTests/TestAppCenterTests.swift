46b42041dd390abba58b150ce14f2bf2a8608e97
mmorenoivy <morenomt27@gmail.com>
2018-11-01 15:22:44 -0500

    commit
    commit

---
+++ TestAppCenterTests/TestAppCenterTests.swift
@@ -0,0 +1,34 @@
+//
+//  TestAppCenterTests.swift
+//  TestAppCenterTests
+//
+//  Created by Ivy, Marjorie on 11/1/18.
+//  Copyright © 2018 Ivy, Marjorie. All rights reserved.
+//
+
+import XCTest
+@testable import TestAppCenter
+
+class TestAppCenterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
